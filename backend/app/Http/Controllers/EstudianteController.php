703a2ea9aea0de73861850c62b68eb72cf7d9f4c
SrDeLorean <xebaelvemgador@gmail.com>
2020-05-01 16:59:57 -0400

    Crud de estudiante
    Nota: php artisan db:seed

---
+++ backend/app/Http/Controllers/EstudianteController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Estudiante;
+use Illuminate\Http\Request;
+
+class EstudianteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $estudiantes = Estudiante::all();
+        return $estudiantes;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+        $estudiante = Estudiante::create($request->all());
+        return $estudiante;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $estudiante = Estudiante::find($id);
+        return $estudiante;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $estudiante = Estudiante::find($id);
+        return $estudiante;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $estudiante = Estudiante::find($id);
+        $estudiante->name = $request->name;
+        $estudiante->save();
+        return $estudiante;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $estudiante = Estudiante::find($id);
+        $estudiante->delete();
+    }
+}
