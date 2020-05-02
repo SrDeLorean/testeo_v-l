08a65158ae8c937901342589a0a51117b019080e
felipe <felipe.milla.calquin@gmail.com>
2020-05-01 21:40:46 -0400

    funciona globalmente
    this

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,15 +2,29 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+import axios from 'axios'
 
 export default new Vuex.Store({
   state: {
+    status:''
 
   },
   mutations: {
+    login(state,lista){
+      console.log(lista.email);
+      console.log(lista.pass);
+      let post ={
+        "email": lista.email,
+        "password": lista.pass,
+      };
+      axios.post('http://127.0.0.1:8000/api/v1/auth/login',post).then((result)=>{
+        //console.log(result)
+        console.log(result.statusText);
+        state.status = result.statusText; 
+      })
+    }
   },
   actions: {
-    
   },
   modules: {
   }
